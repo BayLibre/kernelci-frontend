8eba8cc2ede7c11582ec7372221195240065c92a
Kevin Hilman <khilman@baylibre.com>
2018-10-10 10:41:44 +0200

    bump version

--- app/dashboard/static/js/common.js
+++ app/dashboard/static/js/common.js
@@ -83,7 +83,7 @@ require.config({
             'app/view-tests-board-job': 'app/view-tests-board-job.2017.12',
             'app/view-tests-board-job-kernel': 'app/view-tests-board-job-kernel.2017.12',
             'app/view-tests-suite-id': 'app/view-tests-suite-id.2018.2',
-            'app/view-releases-all': 'app/view-releases-all.2018.09.10',
+            'app/view-releases-all': 'app/view-releases-all.2018.10.11',
             'app/view-releases-kernel': 'app/view-releases-kernel.2018.09.10',
             'app/view-releases-kernel-board': 'app/view-releases-kernel-board.2018.09.10',
             'app/view-releases-kernel-board-suite-name': 'app/view-releases-kernel-board-suite-name.2018.09.10',
